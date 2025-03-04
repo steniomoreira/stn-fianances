f5ca8e6f1dd7be3dc677c66528ee5bd93633952f
steniomoreira <steniomoreiradev@gmail.com>
2025-03-04 15:56:10 -0300

    feat: add month select in header dashboard

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,6 +2,14 @@ import { EllipsisVertical } from "lucide-react";
 
 import Headline from "@/components/headline";
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 import { BarChartReneuExpense } from "./_components/chart/bar-cart-revenue-expense";
 import { LineChartExpense } from "./_components/chart/line-chart-expense";
@@ -10,8 +18,23 @@ import LastTransactions from "./_components/transaction/last-transactions";
 
 export default function DashboardPage() {
   return (
-    <div className="flex flex-col p-4">
-      <Headline text="Dashboard" />
+    <div className="flex flex-col space-y-4 p-4">
+      <div className="flex items-center justify-between">
+        <Headline text="Dashboard" />
+
+        <Select defaultValue="3">
+          <SelectTrigger className="w-[160px] rounded-full">
+            <SelectValue placeholder="Qual mês?" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="1">Janeiro/2025</SelectItem>
+              <SelectItem value="2">Fevereiro/2025</SelectItem>
+              <SelectItem value="3">Março/2025</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
 
       <div className="grid grid-cols-9 gap-4">
         <div className="col-span-6 space-y-4">
