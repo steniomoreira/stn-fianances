36223eab10d287547375686a53153d978cd2eb44
steniomoreira <steniomoreiradev@gmail.com>
2025-03-20 00:39:56 -0300

    feat: add login with credentils with next auth

---
+++ auth.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+import { findUserByEmailAndPassword } from "@/services/users/findUserByEmailAndPassword";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const { email, password } = credentials;
+
+        const user = await findUserByEmailAndPassword(
+          String(email),
+          String(password),
+        );
+
+        return user;
+      },
+    }),
+  ],
+});
