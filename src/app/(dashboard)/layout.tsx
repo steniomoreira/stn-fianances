79378b56beae29a31e0dd9618613657bb253c82f
steniomoreira <steniomoreiradev@gmail.com>
2025-03-23 13:42:34 -0300

    feat: add session auth in dashboard layout component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,22 @@
+import { redirect } from "next/navigation";
+
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
+import { auth } from "../../../auth";
 import Header from "./_components/header/header";
 import { AppSidebar } from "./_components/sidebar/sidebar";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
   return (
     <SidebarProvider>
       <AppSidebar />
